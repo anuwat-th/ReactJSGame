270ad77ccf1956833db2c66a3fc6b71aedfa85c1
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-02-07 22:17:30 +0700

    OK

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import ApplicationRoute from "./routes/ApplicationRoute"
 
 const rootElement = document.getElementById('root');
