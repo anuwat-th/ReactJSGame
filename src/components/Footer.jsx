95a728b0dd73d17f5d56f03eb4f5b0671586c9a4
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-02-07 21:40:45 +0700

    OK

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ import "../style/components/FooterStyle.scss"
 const Footer = () => {
     return (
         <div className="Footer">
-
+            <p className="footerPrimary">นายอนุวัฒน์ ทองคำ&nbsp;<span>643020060-0</span></p>
+            <p className="footerSecondary">College of Computing, Khon Kaen University</p>
         </div>
     );
 }
