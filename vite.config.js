1199873b3d03a4ec7b2d87b3a13d0f5d8fa696ed
“anuwat-thongkham” <anuwat.one@icloud.com>
2024-02-10 16:07:45 +0700

    OK

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ReactJSGame/",
+  server: {
+    host: true,
+  }, 
+  build: {
+    outDir: 'build', // Specify the output directory
+  },
 })
